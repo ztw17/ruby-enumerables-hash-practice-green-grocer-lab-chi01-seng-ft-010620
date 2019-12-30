1d3379039a75fe2710e5883116061037c1f6fbd2
Zach Weber <weberzt@gmail.com>
2019-12-30 23:40:28 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -46,5 +46,13 @@ end
 
 def checkout(cart, coupons)
   consol_cart = consolidate_cart(cart)
-  cart_with_coupons_applied = apply_coupons(consol_cart, coupons )
+  cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
+  cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
+  
+  total = 0.0 
+  cart_with_discounts_applied.keys.each do |item|
+    total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
+  end
+  total > 100.00 ? (total * 0.90).round : total 
+end
 end
