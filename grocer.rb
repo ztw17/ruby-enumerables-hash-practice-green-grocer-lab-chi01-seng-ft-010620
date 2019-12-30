cbbf206473b5cbb658caf04815f38c31436d98bb
Zach Weber <weberzt@gmail.com>
2019-12-30 23:18:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,7 +2,8 @@ def consolidate_cart(cart)
   new_cart = {}
   cart.each do |items_array|
     items_array.each do |item, price_hash|
-      new_cart[item] || 
+      new_cart[item] ||= price_hash
+      
 end
 
 def apply_coupons(cart, coupons)
